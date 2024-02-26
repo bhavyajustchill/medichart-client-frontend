fade3030b28d8f6b9829d2d22e3643be5a20ad04
shahnawazbheda <shahnawazbheda@gmail.com>
2024-02-26 20:12:25 +0530

    Registration page Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,40 +4,23 @@ export default function Login() {
   return (
     <>
       <div className="flex items-center justify-center " style={{
-        backgroundImage: "url('/assets/login-background.jpg')", backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', height: '100dvh', 
+        backgroundImage: "url('/assets/login-background.jpg')", backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', height: '100dvh',
       }}>
 
-        <div className="card lg:card-side bg-base-100 shadow-xl w-1/2 " >
-          <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
-          <div className="card-body">
-            <h2 className="card-title">New album is released!</h2>
-            <p>Click the button to listen on Spotiwhy app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
+        <div className="card lg:card-side bg-base-100 shadow-2xl w-1/2 h-80 " >
+          <figure className="w-1/2"><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+          <div className="card-body ">
+            <p className="text-center font-bold text-3xl text-blue-400">Health card</p>
+            <input type="text" placeholder="Enter Mobile No." className="input w-full border border-blue-400" />
+            <div className="p-8">
+              <button className="btn text-white  hover:bg-blue-400 bg-blue-400 w-full">Get OTP</button>
+              <p className="text-xs mt-2 text-blue-400"><a href="/auth/register" >Create Account</a></p>
             </div>
+           
           </div>
         </div>
       </div>
 
- {/* <div className="flex items-center justify-center" style={{
-        backgroundImage: "url('/assets/login-background.jpg')",
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% 100%',
-        height: '100vh',
-        
-      }}>
-
-        <div className="card lg:card-side bg-base-100 shadow-xl w-1/2">
-          <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" style={{ filter: 'brightness(100%)' }} /></figure>
-          <div className="card-body">
-            <h2 className="card-title">New album is released!</h2>
-            <p>Click the button to listen on Spotiwhy app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,26 @@ import React from "react";
 export default function Register() {
   return (
     <>
-      
+      <div className="flex items-center justify-center " style={{
+        backgroundImage: "url('/assets/login-background.jpg')", backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', height: '100dvh',
+      }}>
+
+        <div className="card lg:card-side bg-base-100 shadow-2xl w-1/2  " >
+          <div className="card-body ">
+            <p className="text-center font-bold text-3xl text-blue-400">Registration</p>
+            <input type="text" placeholder="Enter Name" className="input w-full border border-blue-400 h-7 mt-5" />
+            <input type="text" placeholder="Enter Mobile No." className="input w-full border border-blue-400 h-7 mt-2" />
+            <input type="text" placeholder="Enter Email" className="input mt-2 w-full border border-blue-400 h-7" />
+            <input type="text" placeholder="Enter Adharcard No." className="input mt-2 w-full border border-blue-400 h-7" />
+            <textarea type="text" placeholder="Enter Address"  rows={3} className="input mt-2 w-full border border-blue-400 h-fit" />
+            <div className="p-8 flex flex-col justify-center items-center">
+              <button className=" text-white  hover:bg-blue-400 bg-blue-400 w-1/3 h-8 rounded-xl">Submit</button>
+              <p className="text-xs mt-2 text-blue-400"><a href="/auth/login">Already Exist</a></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
