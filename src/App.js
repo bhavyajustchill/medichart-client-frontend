22554dffd2d3c72910221e85ef72dde77eccfea0
Bhavya Popat <bhavyabpopat7777@gmail.com>
2024-02-26 10:38:17 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="prose prose-base">
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
