d36c06062666e66c4f93f70dd35e3a7033608992
shahnawazbheda <shahnawazbheda@gmail.com>
2024-02-28 16:11:01 +0530

    Registration page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,8 +19,32 @@ export default function Login() {
            
           </div>
         </div> */}
-        <div className="card lg:card-side bg-base-100 shadow-2xl md:w-1/2 md:h-80  " >
-          <figure className="md:w-1/2"><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+        <div className="card lg:card-side bg-base-100 shadow-2xl w-1/2 md:h-80  " >
+          <figure className="md:w-1/2">
+            <div className="w-full carousel rounded-box">
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+              <div className="carousel-item w-full">
+                <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+              </div>
+            </div>
+          </figure>
           <div className="card-body ">
             <p className="text-center font-bold text-3xl text-blue-400">Health card</p>
             <input type="email" placeholder="Enter Email ID." className="input w-full border border-blue-400 h-10" />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,11 +10,34 @@ export default function Register() {
         <div className="card lg:card-side bg-base-100 shadow-2xl w-1/2  " >
           <div className="card-body ">
             <p className="text-center font-bold text-3xl text-blue-400">Registration</p>
-            <input type="text" placeholder="Enter Name" className="input w-full border border-blue-400 h-8 mt-5" />
-            <input type="text" placeholder="Enter Mobile No." className="input w-full border border-blue-400 h-8 mt-2" />
-            <input type="text" placeholder="Enter Email" className="input mt-2 w-full border border-blue-400 h-8" />
-            <input type="text" placeholder="Enter Adharcard No." className="input mt-2 w-full border border-blue-400 h-8" />
-            <textarea type="text" placeholder="Enter Address"  rows={3} className="input mt-2 w-full border border-blue-400 h-fit" />
+            <div className="flex">
+              <input type="text" placeholder=" First Name" className="input w-full border border-blue-400 h-8 mt-5" />
+              <input type="text" placeholder=" Last Name" className="input ms-3 w-full border border-blue-400 h-8 mt-5" />
+            </div>
+            <div className="flex">
+              <input type="text" placeholder=" UserName" className="input w-full border border-blue-400 h-8 " />
+              <input type="text" placeholder=" Mobile No." className="input  ms-3 w-full border border-blue-400 h-8 " />
+            </div>
+
+            <div className="flex">
+              <input type="text" placeholder=" Email" className="input  w-full border border-blue-400 h-8" />
+              <input type="text" placeholder=" Adharcard No." className="ms-3 input  w-full border border-blue-400 h-8" />
+            </div>
+            <div className="flex">
+              <input type="password" placeholder=" Password" className="input  w-full border border-blue-400 h-8" />
+              <input type="password" placeholder="Confirm Password" className="ms-3 input  w-full border border-blue-400 h-8" />
+            </div>
+
+            <div className="flex">
+              <input type="date" placeholder=" Select Date" className="input  w-72 border border-blue-400 h-8" />
+              <div className="flex ms-5 mt-2">
+                <input type="radio" name="radio-1" className="radio h-5 w-5" checked />
+                <lable className="ms-1">Male</lable>
+                <input type="radio" name="radio-1" className="radio h-5 w-5 ms-5" />
+                <lable className="ms-1">Female</lable>
+              </div>
+            </div>
+            <textarea type="text" placeholder=" Address" rows={3} className="input mt-2 w-full border border-blue-400 h-fit" />
             <div className="p-8 flex flex-col justify-center items-center">
               <button className=" text-white  hover:bg-blue-400 bg-blue-400 w-1/3 h-8 rounded-xl">Submit</button>
               <p className="text-xs mt-2 text-blue-400"><a href="/auth/login">Already Exist</a></p>
